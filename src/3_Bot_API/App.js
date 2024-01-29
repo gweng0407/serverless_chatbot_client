9016b936a1bcb4fc294c7839ca6c5fd64bda8c3a
최민철 <minchul.choi@choemincheols-MacBook-Pro.local>
2024-01-30 00:12:30 +0900

    restructure for AWS CodePipeline

---
+++ src/3_Bot_API/App.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, useRef } from "react";
+import "./App.css";
+
+const LAMBDA_POST_MSG = "<<YOUR LAMBDA FUNCTION URL>>";
+
+function App() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const callApi = async (url, method, body) => {
+    try {
+      const headers = {
+        "Content-Type": "application/json",
+      };
+      const payload = body
+        ? { method, headers, body: JSON.stringify(body) }
+        : { method, headers };
+      const response = await fetch(url, payload);
+      if (!response.ok) {
+        setMessages(messages);
+        console.log("API call failed");
+        console.log(response);
+        return;
+      }
+      return response;
+    } catch (error) {
+      console.error("API call error:", error);
+      setMessages(messages);
+    }
+  };
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (!newMessage) return;
+
+    const message = { text: newMessage, sender: "user" };
+    setMessages([...messages, message]);
+    setNewMessage("");
+
+    callApi(LAMBDA_POST_MSG, "POST", { content: newMessage })
+      .then((res) => res.json())
+      .then((res) => {
+        const parrotResponse = { text: res, sender: "bot" };
+        setMessages((msgs) => [...msgs, parrotResponse]);
+      });
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="messages-container">
+        {messages.map((msg) => (
+          <div key={msg.id} className={`message ${msg.sender}`}>
+            <p>{msg.text}</p>
+          </div>
+        ))}
+        <div ref={messagesEndRef} /> {}
+      </div>
+      <form className="message-form" onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          placeholder="Type a message..."
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
